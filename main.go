0dab324b174abeaf0ae2231658333fd621f88698
pkwiatek6 <pkwiatek6@gmail.com>
2024-11-20 20:25:31 -0500

    Updated error message

--- main.go
+++ main.go
@@ -194,7 +194,7 @@ func init() {
 	log.Println("Connection to Database established")
 	Characters, err = actions.LoadAllCharacters(Client)
 	if err != nil {
-		log.Fatalln("Error loading all characters", err)
+		log.Fatalf("Error loading all characters. ERROR:[%s]\n", err)
 	}
 	log.Println("All Characters loaded")
 
